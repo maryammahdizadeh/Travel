e5e457afa6d1840e50e200f0174f7ae684a0bb13
maryam <maryam19.mahdizadeh@gmail.com>
2024-12-01 22:39:39 +0330

    add service layer and DemoService

--- src/main/java/com/spring/core/controller/DemoController.java
+++ src/main/java/com/spring/core/controller/DemoController.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class DemoController {
+
+    public String hello() {
+        return "hello controller";
+    }
 }
---
+++ src/main/java/com/spring/core/sevice/DemoService.java
@@ -0,0 +1,11 @@
+package com.spring.core.sevice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String hello() {
+        return "hello service";
+    }
+}
