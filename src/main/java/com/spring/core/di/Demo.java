d6faacede6eca3f1001c28bfc75f8383b8cbdc9f
maryam <maryam19.mahdizadeh@gmail.com>
2024-12-30 23:43:11 +0330

    use setter-based DI for optional dependencies

--- src/main/java/com/spring/core/di/Demo.java
+++ src/main/java/com/spring/core/di/Demo.java
@@ -18,9 +18,22 @@ public class Demo {
     private DependencyB dependencyB;
     private DependencyC dependencyC;
 
-    public Demo(DependencyA dependencyA, DependencyB dependencyB, DependencyC dependencyC) {
+//    public Demo(DependencyA dependencyA, DependencyB dependencyB, DependencyC dependencyC) {
+//        this.dependencyA = dependencyA;
+//        this.dependencyB = dependencyB;
+//        this.dependencyC = dependencyC;
+//    }
+
+
+    public void setDependencyA(DependencyA dependencyA) {
         this.dependencyA = dependencyA;
+    }
+
+    public void setDependencyB(DependencyB dependencyB) {
         this.dependencyB = dependencyB;
+    }
+
+    public void setDependencyC(DependencyC dependencyC) {
         this.dependencyC = dependencyC;
     }
 }
